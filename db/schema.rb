64c6dd8657fefa3e29f981955470e06891c8ee80
Harold <155407111+DevHarold0-0@users.noreply.github.com>
2025-11-01 14:55:40 -0400

    Add basic inventory tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_26_220906) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_01_184647) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
